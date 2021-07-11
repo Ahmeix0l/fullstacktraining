156dd14c56e3fb5e016790bc9ed8cd31180d0e55
Ahmeix0l <86569189+Ahmeix0l@users.noreply.github.com>
2021-07-11 10:11:20 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,13 @@
+package knight.day6;
+
+public class DoWhileLoop {
+	public static void main(String args[]) { 
+		 int x = 1; 
+		 
+		 do { 
+		 System.out.print("value of x : " + x ); 
+		 x++; 
+		 System.out.print("\n"); 
+		 }while( x < 11 ); 
+		 } 
+}
