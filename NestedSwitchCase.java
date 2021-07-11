19ceb1fd36a93b468b1a11ebd9bcb7fd62aa8db0
Ahmeix0l <86569189+Ahmeix0l@users.noreply.github.com>
2021-07-11 10:19:28 +0530

    Add files via upload

---
+++ NestedSwitchCase.java
@@ -0,0 +1,22 @@
+package knight.day6;
+
+public class NestedSwitchCase {
+	public static void main(String args[]){ 
+		 int tech = 2; 
+		int course = 2; 
+		 
+		 switch(tech){ 
+		case 1: 
+		 System.out.println("python"); 
+		 break; 
+		 case 2: 
+		 switch(course){ 
+		case 1: 
+		 System.out.println("J2EE"); 
+		 break; 
+		 case 2: 
+		 System.out.println("advance java"); 
+		 } 
+		 } 
+		 }
+}
---
+++ SwitchCase.java
@@ -0,0 +1,22 @@
+package knight.day6;
+
+public class SwitchCase {
+	public static void main(String args[]){ 
+		 int tech = 2; 
+		 
+		 switch(tech){ 
+		case 1: 
+		 { 
+		 System.out.println("java"); 
+		 break; 
+		 } 
+		 case 2:{ 
+		 System.out.println("ES6"); 
+		 break; 
+		 } 
+		 default:{ 
+		 System.out.println("Not Listed.."); 
+		 } 
+		 }
+	}
+}
